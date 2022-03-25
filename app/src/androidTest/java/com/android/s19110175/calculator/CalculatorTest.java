67ae15689ebdcca6b8a561697e0d4e4d1c1ad318
User <tatchu6878@gmail.com>
2022-03-25 13:56:17 +0700

    Initial commit

---
+++ app/src/androidTest/java/com/android/s19110175/calculator/CalculatorTest.java
@@ -0,0 +1,19 @@
+package com.android.s19110175.calculator;
+
+import junit.framework.TestCase;
+
+public class CalculatorTest extends TestCase {
+
+    public void testAddTwoNumbers() {
+        as
+    }
+
+    public void testSubTwoNumbers() {
+    }
+
+    public void testMulTwoNumbers() {
+    }
+
+    public void testDivTwoNumbers() {
+    }
+}
---
+++ app/src/main/java/com/android/s19110175/calculator/Calculator.java
@@ -0,0 +1,22 @@
+package com.android.s19110175.calculator;
+
+public class Calculator {
+
+    public double addTwoNumbers(double a, double b){
+        return a + b;
+    }
+
+    public double subTwoNumbers(double a, double b){
+        return a - b;
+    }
+
+    public double mulTwoNumbers(double a, double b){
+        return a * b;
+    }
+
+    public double divTwoNumbers(double a, double b){
+        return a / b;
+    }
+
+
+}
