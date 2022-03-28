4fa0775efeb8b5f5a2a3bf06b147ff11fa471aa6
User <tatchu6878@gmail.com>
2022-03-28 08:53:13 +0700

    commit test case

--- app/src/main/java/com/android/s19110175/calculator/Calculator.java
+++ app/src/main/java/com/android/s19110175/calculator/Calculator.java
@@ -1,5 +1,6 @@
 package com.android.s19110175.calculator;
 
+
 public class Calculator {
 
     public double addTwoNumbers(double a, double b){
@@ -18,5 +19,33 @@ public class Calculator {
         return a / b;
     }
 
+    public Double exponentialNumber(double a, double b){
+        if(a < 0 || a == 1){
+            return null;
+        }
+        return Math.pow(a,b);
+    }
+
+    public Integer factorialNumber(int a) throws ExceptionInInitializerError{
+        if (a > 0){
+            Integer result = 1;
+
+            for (int i = 1; i <= a; i++){
+                result = result * i;
+            }
+
+            return result;
+        }
+        throw new ExceptionInInitializerError();
+    }
+
+    public double logarithmNumber(double a, double x) throws ExceptionInInitializerError{
+
+        if(a > 0 && a != 1){
+            return Math.log(x) / Math.log(a);
+        }
+
+        throw new ExceptionInInitializerError();
+    }
 
 }
